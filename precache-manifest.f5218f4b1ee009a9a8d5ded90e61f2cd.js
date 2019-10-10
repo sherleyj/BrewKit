e96c04255f7760b0b3c4d8ae27bc2d18e42f6cb1
Sherley Gionfriddo <sgionfriddo@Sherleys-MacBook-Pro.local>
2019-10-09 20:18:48 -0500

    Updates

---
+++ precache-manifest.f5218f4b1ee009a9a8d5ded90e61f2cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19b1b9122e7db06f3e15e07961c6bf0b",
+    "url": "/BrewKit/index.html"
+  },
+  {
+    "revision": "4c4c46c08f6f837069fd",
+    "url": "/BrewKit/static/css/main.20b37a9d.chunk.css"
+  },
+  {
+    "revision": "91bbf5a2f2e353c2f845",
+    "url": "/BrewKit/static/js/2.2ec475fb.chunk.js"
+  },
+  {
+    "revision": "4c4c46c08f6f837069fd",
+    "url": "/BrewKit/static/js/main.eaeae026.chunk.js"
+  },
+  {
+    "revision": "e99f1a115a336b29bc92",
+    "url": "/BrewKit/static/js/runtime~main.276a2c14.js"
+  }
+]);
